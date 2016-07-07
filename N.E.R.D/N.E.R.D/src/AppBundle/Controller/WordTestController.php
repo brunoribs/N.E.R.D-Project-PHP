4afd5f7cbb5923bef08a6de8cc546a5071b12a65
vitorguimars <vitorguima.rs@bol.com.br>
2016-07-06 22:37:53 -0300

    Adicionado phppetclinic com banco funcionando. [Ricardo/Vitor/Eduardo]

---
+++ N.E.R.D/N.E.R.D/src/AppBundle/Controller/WordTestController.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Guilherme
+ * Date: 03/07/2016
+ * Time: 21:53
+ */
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+/**
+ * Get all words (test purpose).
+ *
+ * @Route("/{_locale}/allWords", defaults={"_locale"="en"}, requirements = { "_locale" = "en|de" })
+ */
+class WordTestController extends Controller
+{
+    /**
+     * Word test.
+     *
+     * @Route("/", name="teste")
+     * @Method("GET")
+     * @Template()
+     */
+    public function getAllWordsAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('AppBundle:Word')->findAll();
+        $serializer = new Serializer(
+            array(new GetSetMethodNormalizer(), new ArrayDenormalizer()),
+            array(new JsonEncoder())
+        );
+        $data = $serializer->serialize($entities, 'json');
+        return new Response($data);
+    }
+}
